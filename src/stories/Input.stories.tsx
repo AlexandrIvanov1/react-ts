4f0847971f363410ae12189447426d8a6733a4c9
AlexandrIvanov <alexandr.1996@list.ru>
2022-12-07 03:00:54 +0400

    Using storybook

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -32,3 +32,40 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
         <button onClick={saveChanges}>save</button> - actual value: {value}
     </div>
 }
+
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
+    return <input value={value} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [value, setValue] = useState(false)
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.checked)
+    }
+
+    return <input type="checkbox" checked={value} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [value, setValue] = useState<undefined | string>()
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setValue(e.currentTarget.value)
+    }
+
+    return (
+        <select name="name" id="1" value={value} onChange={onChange}>
+            <option>null</option>
+            <option value="1">Moscow</option>
+            <option value="2">Baki</option>
+            <option value="3">New-York</option>
+        </select>
+    )
+}
